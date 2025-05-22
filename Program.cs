8e4d016aba59afd6ffc12808031d9fb8aa46230c
Cj Perea <cjperea@Cjs-MacBook-Pro.local>
2025-05-21 23:23:48 -0600

    finshed lab 1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Learning how to do it
+Console.WriteLine("Hello, World!");
+
+string aFriend = "Bobby";
+Console.WriteLine(aFriend);
+
+aFriend = "Robby";
+Console.WriteLine("Hello, " + aFriend);
+
+Console.WriteLine($"Hi {aFriend}");
+
+string firstFriend = "liz";
+string secondFriend = "kate";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+string songLyrics = "You say goodbye, and I say greetings";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
+
+//The challenge
